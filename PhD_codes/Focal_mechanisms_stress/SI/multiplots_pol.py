8998e11fd613703c8ec66125a7e272fa6915eab4
kemichai <konstantinos.michailos@gmail.com>
2022-05-06 12:41:13 +0200

    updated code for plotting focal mec details

--- PhD_codes/Focal_mechanisms_stress/SI/multiplots_pol.py
+++ PhD_codes/Focal_mechanisms_stress/SI/multiplots_pol.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python
+"""
+Read quakeml catalog of earthquakes with focal mechanisms and
+plot details.
+
+May 2022
+Konstantinos Michailos
+"""
 from obspy import read_events
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+import os
 
-# Read the .npz dictionary
-# path='/home/michaiko/Dropbox/Dropbox/VUW/PhD/Catalogs/Parameters/'
-# parameters = np.load(path+"Focal_mechanisms_parameters.npz")
-# parameters.keys()
-# lats = parameters["latitude"]
+# Define current directory
+WORK_DIR = os.getcwd()
+
+CAT_PATH = '/'.join(WORK_DIR.split('/')[0:-1]) + '/quakeml_file/Final_foc_mec_catalog.xml'
 
 print('>>> Reading catalog...')
-cat = read_events('/Volumes/GeoPhysics_05/users-data/michaiko/Data/Catalogs/'
-                   'Final_foc_mec_catalog.xml')
+cat = read_events(CAT_PATH)
 print('>>> Done.')
 
 strike = []
@@ -32,45 +39,6 @@ for ev in cat:
     mag.append(float(ev.magnitudes[0].mag))
     dep.append(float(ev.origins[-1].depth)/1000)
     
-#     try:
-#         dep.append(float(ev.origins[2].depth)/1000)
-#     except Exception as e:
-#         print(e)
-#         print('No 3rd origin')
-#         dep.append(float(ev.origins[-1].depth)/1000)
-
-# # Write the gmt input
-# for ev in cat:
-#     if ev.origins[-2].creation_info.version == 'HypoDD':
-#         orig = ev.origins[2].time
-#         yr = str(orig)[0:4]
-#         mo = str(orig)[5:7]
-#         da = str(orig)[8:10]
-#         hr = str(orig)[11:13]
-#         mn = str(orig)[14:16]
-#         se = str(orig)[17:19]
-#         se2 = str(orig)[20:22]
-#         time = yr + mo + da + '.' + hr + mn + se + se2
-        
-#         lon = ev.origins[2].longitude
-#         dep = ev.origins[2].depth/1000
-#         lat = ev.origins[2].latitude
-#         strike = ev.focal_mechanisms[0].nodal_planes.\
-#                             nodal_plane_1.strike
-#         dip = ev.focal_mechanisms[0].nodal_planes.nodal_plane_1.dip
-#         rake = ev.focal_mechanisms[0].nodal_planes.nodal_plane_1.rake
-#         error = float(ev.focal_mechanisms[0].comments[0].text)
-#         mag = ev.magnitudes[-1].mag
-
-
-#         with open('foc_mec_gmt_input.dat', 'a') as of:
-#             of.write('{} {} {} {} {} {} {} 0 {} {}\n'.format(lon, lat, dep,
-#                       strike, dip, rake, mag, error, time))
-
-
-
-
-
 font = {'family': 'normal',
         'weight': 'normal',
         'size': 18}
@@ -83,43 +51,35 @@ fig_size[1] = 17
 plt.rcParams["figure.figsize"] = fig_size
 
 
-
 ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=1)
-ax1.scatter(mag, unce, facecolor='darkgrey', alpha=0.6, edgecolor='black',
-            linewidth=1.)
+ax1.scatter(mag, unce, facecolor='darkgrey', alpha=0.6, edgecolor='black', linewidth=1.)
 ax1.set_ylabel('Focal Mechanism \n error ($^\circ$)', fontsize=20)
 ax1.set_xlabel('Magnitudes (M$_L$)', fontsize=20)
 plt.xticks(np.arange(1.5, 5.5, 1))
 plt.yticks(np.arange(20,45, 5))
 
 ax2 = plt.subplot2grid((3, 3), (0, 1), colspan=1)
-ax2.scatter(npol, unce, facecolor='darkgrey', alpha=0.6, edgecolor='black',
-            linewidth=1.)
+ax2.scatter(npol, unce, facecolor='darkgrey', alpha=0.6, edgecolor='black', linewidth=1.)
 plt.yticks(np.arange(20,45, 5))
 plt.xticks(np.arange(7, 40, 6))
 
-
 ax3 = plt.subplot2grid((3, 3), (0, 2), colspan=1)
-ax3.scatter(azim, unce, facecolor='darkgrey', alpha=0.6, edgecolor='black',
-            linewidth=1.)
+ax3.scatter(azim, unce, facecolor='darkgrey', alpha=0.6, edgecolor='black', linewidth=1.)
 plt.xticks(np.arange(60, 361, 60))
 plt.yticks(np.arange(20,45, 5))
-
+#
 ax4 = plt.subplot2grid((3, 3), (1, 1), colspan=1)
-ax4.scatter(npol, mag, facecolor='darkgrey', alpha=0.6, edgecolor='black',
-            linewidth=1.)
+ax4.scatter(npol, mag, facecolor='darkgrey', alpha=0.6, edgecolor='black', linewidth=1.)
 ax4.set_ylabel('Magnitudes (M$_L$)', fontsize=20)
 ax4.set_xlabel('Number of \n polarities', fontsize=20)
 plt.xticks(np.arange(7, 40, 6))
 plt.yticks(np.arange(1.5, 5.5, 1))
-
-
+#
 ax5 = plt.subplot2grid((3, 3), (1, 2), colspan=1)
-ax5.scatter(azim, mag, facecolor='darkgrey', alpha=0.6, edgecolor='black',
-            linewidth=1.)
+ax5.scatter(azim, mag, facecolor='darkgrey', alpha=0.6, edgecolor='black', linewidth=1.)
 plt.yticks(np.arange(1.5, 5.5, 1))
 plt.xticks(np.arange(60, 361, 60))
-
+#
 ax6 = plt.subplot2grid((3, 3), (2, 2), colspan=1)
 ax6.scatter(azim, npol, facecolor='darkgrey', alpha=0.6, edgecolor='black',
             linewidth=1.)
@@ -127,11 +87,10 @@ ax6.set_xlabel('Focal Mechanism \n azimuthal gap ($^\circ$)', fontsize=20)
 ax6.set_ylabel('Number of \n polarities', fontsize=20)
 plt.yticks(np.arange(7, 40, 6))
 plt.xticks(np.arange(60, 361, 60))
-
 plt.show()
 
 
-
+##
 ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=1)
 ax1.scatter(mag, rake, facecolor='darkgrey', alpha=0.6, edgecolor='black',
             linewidth=1.)
@@ -141,7 +100,7 @@ ax1.set_ylim([-180, 180])
 ax1.set_ylim([1.5, 5.5])
 plt.xticks(np.arange(1.5, 5.5, 1))
 plt.yticks(np.arange(-180,181, 60))
-
+#
 ax2 = plt.subplot2grid((3, 3), (0, 1), colspan=1)
 ax2.scatter(dep, rake, facecolor='darkgrey', alpha=0.6, edgecolor='black',
             linewidth=1.)
@@ -149,7 +108,7 @@ plt.yticks(np.arange(-180,181, 60))
 plt.xticks(np.arange(0, 31, 10))
 ax2.set_ylim([-180, 180])
 ax2.set_xlim([0, 30])
-
+#
 ax4 = plt.subplot2grid((3, 3), (1, 1), colspan=1)
 ax4.scatter(dep,mag, facecolor='darkgrey', alpha=0.6, edgecolor='black',
             linewidth=1.)
@@ -158,7 +117,6 @@ ax4.set_xlabel('Hypocentral \n depths (km)', fontsize=20)
 ax4.set_xlim([0, 30])
 plt.xticks(np.arange(0, 31, 10))
 plt.yticks(np.arange(1.5, 5.5, 1))
-
 plt.show()
 
 
