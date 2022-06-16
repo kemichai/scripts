780c015d1e00a7f02e9c85ab1b00104a89f4cf02
kmichailos <17120684+kemichai@users.noreply.github.com>
2022-06-16 16:27:08 +0200

    added a code that reads quakeml files to store eq parameters in npz format; for reading catalog faster

--- PhD_codes/other/quakeml2npz.py
+++ PhD_codes/other/quakeml2npz.py
@@ -4,27 +4,16 @@
 
 """
 from obspy import read_events
-from progressbar import ProgressBar
 import numpy as np
 
 
-def read_cat4vars(cat_path):
+def read_cat4vars(cat_path, output_filename='parameters.npz'):
     """
     Function to read *.xml earthquake catalogs and create lists of the eq parameters (e.g. Lat, Lon, Dep, etc.)
     and save these values in a dictionary.
 
     :param cat_path: Path to the quakeml file.
     :type cat_path: str
-
-    Example:
-    To read the dictionary use:
-    >>> parameters = np.load("parameters.npz")
-    >>> parameters.items()
-    # This will output the names of the lists...
-    >>> parameters.keys()
-    >>> lats = parameters["latitude"]
-    # etc
-
     """
 
     print('Reading earthquake catalog ' + cat_path.split('/')[-1] + '...')
@@ -38,6 +27,29 @@ def read_cat4vars(cat_path):
     lon = []
     dep = []
     mag= []
+    for ev in cat:
+        # Earthquake location details
+        event_id.append(ev.preferred_origin_id)
+        o = ev.preferred_origin()
+        lat.append(o.latitude)
+        lon.append(o.longitude)
+        dep.append(o.depth)
+        mag.append(float(ev.magnitudes[0].mag))
+        dattime.append(o.time)
+    print('Writing dictionary ...')
+    # Write a dictionary
+    dictionary = {"eq_ids": event_id, "longitudes": lon, "latitudes": lat,
+                  "depths": dep, "magnitudes": mag, "datetimes":dattime}
+    np.savez(output_filename, **dictionary)
+
+    return
 
 
+# Create the npz file 
+a = read_cat4vars('/home/kmichailos/Desktop/codes/github/codes/PhD_codes/Focal_mechanisms_stress/quakeml_file/subset_cat.xml')
+
+# Read the npz file
+parameters = np.load('parameters.npz')
+parameters.items()
+lats = parameters["latitudes"]
 
